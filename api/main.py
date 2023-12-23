004bee1851239e9ecead814845d86323479a83d5
kashiwagi-rena <azisai07@gmail.com>
2023-12-23 18:08:52 +0900

    jinja2で表示できた

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-# from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI(debug=True)
 # app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 @app.route("/hello")
 def read_item(request: Request):
     # テンプレートに渡すデータ
